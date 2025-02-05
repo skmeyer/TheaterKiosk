d4309e40294bd1567e1728be14db2a9ee0444c94
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-04 19:58:11 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TheaterKiosk
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int userAge = 0;
+        int WRIST_BAND_AGE = 21;
+
+        System.out.print("Enter your age: ");
+        userAge = in.nextInt();
+
+    }
+}
